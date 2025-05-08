8f5b8ea13bf52d7e479046cb1a736dd9ad30a297
Gii-th4 <anggitadewlestari@gmail.com>
2025-05-08 14:45:23 +0700

    Resolve conflict in dj,ds,tj

--- admin/data-jurusan.php
+++ admin/data-jurusan.php
@@ -4,15 +4,12 @@ if (!isset($_SESSION['email']) || $_SESSION['role'] != 'admin') {
   header("Location: login.php");
   exit;
 }
-<<<<<<< HEAD
 
 include '../database.php'; 
 $query = "SELECT * FROM jurusan";
 $result = mysqli_query($conn, $query);
 
 
-=======
->>>>>>> 6c6a408cf618b5e4192c6fe45843db72acea1a7d
 ?>
 
 <!DOCTYPE html>
